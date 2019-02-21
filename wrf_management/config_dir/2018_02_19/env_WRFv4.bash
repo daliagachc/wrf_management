bfa4b5c106c918ca84ccdd20a2be678d264d1301
Diego Aliaga <diego.aliaga@helsinki.fi>
2019-02-22 01:53:32 +0200

    sdf

---
+++ wrf_management/config_dir/2018_02_19/env_WRFv4.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export NETCDF=/appl/opt/netcdf4/gcc-7.3.0/intelmpi-18.0.2/4.6.1/
+module purge
+module load gcc/7.3.0  intelmpi/18.0.2 hdf5-par/1.8.20 netcdf4/4.6.1
+export WRFIO_NCD_LARGE_FILE_SUPPORT=1 
+
