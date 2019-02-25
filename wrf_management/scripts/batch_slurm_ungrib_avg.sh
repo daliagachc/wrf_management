dc05ecc6ad647135bbf6c1ed4356594c7ca2c163
diego <diego.aliaga@helsinki.fi>
2019-02-25 23:05:39 +0200

    from_mac

--- wrf_management/scripts/batch_slurm_ungrib_avg.sh
+++ wrf_management/scripts/batch_slurm_ungrib_avg.sh
@@ -2,7 +2,7 @@
 #SBATCH -J array_job
 #SBATCH -o array_job_out_%A_%a.txt
 #SBATCH -e array_job_err_%A_%a.txt
-#SBATCH -t 00:35:00
+#SBATCH -t 00:25:00
 #SBATCH --mem-per-cpu=4000
 #SBATCH --array=1-60
 #SBATCH -n 1
@@ -11,5 +11,5 @@
 sleep $SLURM_ARRAY_TASK_ID
 
 conda activate b36
-python -u ungrib_press-taito.py
+python -u ungrib_avgtsfc.py
 
