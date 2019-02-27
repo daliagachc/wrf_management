0918c9725a9519162ce3f64914221b2ae18db126
Diego Aliaga <diego.aliaga@helsinki.fi>
2019-02-27 19:18:12 +0200

    taito

--- wrf_management/config_dir/run_2019_02_27/run_scripts/Untitled.py
+++ wrf_management/config_dir/run_2019_02_27/run_scripts/Untitled.py
@@ -6,4 +6,12 @@
 #       format_name: percent
 #       format_version: '1.2'
 #       jupytext_version: 1.0.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
+
+# %%
+import wrf_management.utilities as ut
+
