ef86f6147e0fbbb7f65677693508a8c3f7a70c58
diego <diego.aliaga@helsinki.fi>
2019-04-03 17:38:18 +0300

    from_mac

--- wrf_management/config_dir/run_2019_04_03_1/run_scripts/run_real.py
+++ wrf_management/config_dir/run_2019_04_03_1/run_scripts/run_real.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 0.8.6
+#       jupytext_version: 1.0.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -19,12 +19,11 @@ import os
 import init as it
 
 print(it.run_name)
+importlib.reload(it)
 
 import di_python.ya_esta as ye
 import wrf_management.run_wrf as rw
 import wrf_management.project_global_constants as gc
-
-importlib.reload(rw)
 
 # %%
 df = rw.get_date_range(it.init_date, it.end_date)
@@ -35,7 +34,7 @@ input_path = rw.get_input_file_path(it.run_name)
 input_dic = rw.get_input_dic(input_path)
 
 new_input_dic = rw.modify_dt_input_dic(it.init_date, it.end_date, input_dic)
-it.run_type = 'wrf'
+
 run_type_dir = rw.create_run_type_dir(gc.PATH_DATA, it.run_name, it.run_type)
 
 rw.write_input_dic(run_type_dir=run_type_dir, new_input_dic=new_input_dic)
@@ -52,20 +51,17 @@ parent_run_dir
 
 # %%
 dates = list(df.dt)
-dates[0]
+dates
 
 # %%
-importlib.reload(rw)
-real_path = rw.create_run_type_dir(gc.PATH_DATA, it.run_name, 'real')
-print(real_path)
-rw.link_wrfs(real_path=real_path, dest_path=run_type_dir)
+rw.link_metgrids(parent_run_path=parent_run_dir, dates=dates,
+                 dest_path=run_type_dir)
 
 # %%
-importlib.reload(rw)
-rw.link_wrf(dest_path=run_type_dir)
-
-# %%
-rw.cp_sbatch(source_dir='../', target_dir=run_type_dir, pat='*wrf*.sh')
+rw.link_real(dest_path=run_type_dir)
 
 # %%
 
+rw.cp_sbatch(source_dir='../', target_dir=run_type_dir, pat='*real.sh')
+
+# %%
