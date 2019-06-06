3815e75cdc3028ad8cbfe9758bb150293b487eb4
diego <diego.aliaga@helsinki.fi>
2019-06-06 10:55:47 -0400

    from_mac

--- wrf_management/modules/Untitled1.py
+++ wrf_management/modules/Untitled1.py
@@ -6,4 +6,22 @@
 #       format_name: percent
 #       format_version: '1.2'
 #       jupytext_version: 1.1.3
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
 # ---
+
+# %%
+a <- 4
+
+# %%
+a
+
+# %%
+a = 23 
+
+# %%
+asfl = 234 
+
+# %%
